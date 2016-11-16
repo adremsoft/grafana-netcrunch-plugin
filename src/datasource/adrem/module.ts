67d04282d0e0b78634e9b5d7ff1b971f2b03ea20
Bogusław Gorczyca <bgorczyca@adrem.com.pl>
2016-11-16 11:55:40 +0100

    Adrem communication module has been added.

---
+++ src/datasource/adrem/module.ts
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright AdRem Software. All Rights Reserved.
+ *
+ * Use of this source code is governed by an Apache License, Version 2.0 that can be
+ * found in the LICENSE file.
+ */
+
+const adremModuleUrl = 'plugins/grafana-netcrunch/datasource/adrem/',
+      adremClient = adremModuleUrl + 'client.min.js',
+      objectMapper = adremModuleUrl + 'ObjMapper.min.js',
+      remoteDataLists = adremModuleUrl + 'RemoteDataLists.min.js',
+      netCrunchObjects = adremModuleUrl + 'NCObjects.min';
+
+function importAdremClient() {
+  return SystemJS.import(adremClient).then((adrem) => {
+    return SystemJS.import(remoteDataLists).then(() => {
+      return SystemJS.import(objectMapper).then(() => {
+        return SystemJS.import(netCrunchObjects).then(() => {
+          return adrem;
+        });
+      });
+    });
+  });
+}
+
+const adrem = importAdremClient();
+
+export {
+  adrem as adrem
+}
