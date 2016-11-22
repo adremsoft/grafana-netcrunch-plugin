0c579106b43d9895b5fbb4b1a5faf3ef0a14d36a
Bogusław Gorczyca <bgorczyca@adrem.com.pl>
2016-11-22 16:32:08 +0100

    netCrunchAPI has been implemented

--- src/datasource/services/netCrunchAPI/netCrunchAPI.service.ts
+++ src/datasource/services/netCrunchAPI/netCrunchAPI.service.ts
@@ -9,11 +9,84 @@
 import angular from 'angular';
 import {servicesModule} from '../../common';
 import './adrem/module';
+import NetCrunchConnectionCache from './netCrunchConnectionCache';
+import NetCrunchConnection from './netCrunchConnection/netCrunchConnection';
 
 class NetCrunchAPIService {
 
   /** @ngInject */
-  constructor (adrem) {
+  constructor (adrem, alertSrv, $rootScope) {
+    this.adrem = adrem;
+    this.alertSrv = alertSrv;
+    this.$rootScope = $rootScope;
+    this.cache = new NetCrunchConnectionCache();
+  }
+
+  testConnection(datasource) {
+    return this.clearConnection(datasource)
+      .then(() => {
+        return this.getConnection(datasource, true); })
+      .then(() => {
+        return this.clearConnection(datasource); });
+  }
+
+  clearConnection(datasource) {
+    return new Promise((resolve) => {
+      if (this.cache.connectionExist(datasource)) {
+        this.cache.getConnection(datasource).then((connection) => {
+          connection.logout().then(() => {
+            this.cache.deleteConnection(datasource);
+            resolve();
+          });
+        });
+      } else {
+        resolve();
+      }
+    });
+  }
+
+  getConnection(datasource, withoutNetworkAtlas = false) {
+    //TODO: CHECK SERVER VERSION
+    return this.getNetCrunchConnection(datasource, withoutNetworkAtlas);
+  }
+
+  getNetCrunchConnection(datasource, withoutNetworkAtlas = false) {
+    if (this.cache.connectionExist(datasource)) {
+      return this.cache.getConnection(datasource).then((connection) => {
+        connection.fromCache = true;
+        return connection;
+      });
+    } else {
+      let self = this,
+          connection = new NetCrunchConnection(this.adrem, datasource.url, datasource.name);
+
+      connection.onError = function(error) {
+        self.alertSrv.set(error.connectionName, error.message, 'error');
+      };
+
+      connection.onNodesChanged = function() {
+        self.$rootScope.$broadcast('netcrunch-nodes-data-changed(' + datasource.name + ')');
+      };
+
+      connection.onNetworksChanged = function() {
+        self.$rootScope.$broadcast('netcrunch-networks-data-changed(' + datasource.name + ')');
+      };
+
+      this.cache.addConnection(datasource, new Promise((resolve, reject) => {
+        connection.login(datasource.username, datasource.password, withoutNetworkAtlas)
+          .then(() => {
+            connection.fromCache = false;
+            resolve(connection);
+          })
+          .catch((error) => {
+            self.cache.deleteConnection(datasource);
+            connection.logout();
+            reject(error);
+          });
+      }));
+
+      return this.cache.getConnection(datasource);
+    }
   }
 
 }
