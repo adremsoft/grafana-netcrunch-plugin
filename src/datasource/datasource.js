e5fd0632cc5fc4f381b1c734d210baef2babc873
Bogusław Gorczyca <bgorczyca@adrem.com.pl>
2016-12-01 11:38:45 +0100

    datasource module has been refactored

--- src/datasource/datasource.js
+++ src/datasource/datasource.js
@@ -6,60 +6,35 @@
  * found in the LICENSE file.
  */
 
-import {CONNECTION_ERROR_MESSAGES} from './services/netCrunchAPI/module';
+import { CONNECTION_ERROR_MESSAGES } from './services/netCrunchAPI/module';
 
-const SERIES_TYPES_DISPLAY_NAMES = {
-        min : 'Min',
-        avg : 'Avg',
-        max : 'Max',
-        avail : 'Avail',
-        delta : 'Delta',
-        equal : 'Equal',
-        distr : 'Distr'
-      };
+const
+  SERIES_TYPES_DISPLAY_NAMES = {
+    min: 'Min',
+    avg: 'Avg',
+    max: 'Max',
+    avail: 'Avail',
+    delta: 'Delta',
+    equal: 'Equal',
+    distr: 'Distr'
+  };
 
 class NetCrunchDatasource {
 
   /** @ngInject */
   constructor(instanceSettings, netCrunchAPIService, alertSrv, $rootScope) {
+    const
+      self = this;
     let
-      self = this,
       nodesReady,
       atlasReady,
       datasourceInitialization = null;
 
-    function initDatasource() {
-      let netCrunchSession;
+    function initNodesUpdating(networkAtlas, fromCache) {
 
-      return new Promise((resolve, reject) => {
-        if (self.url != null) {
-          netCrunchSession = self.netCrunchAPI.getConnection(self);
-          netCrunchSession
-            .then((connection) => {
-              let fromCache = connection.fromCache;
-              self.netCrunchConnection = connection;
-              initNodesUpdating(connection.networkAtlas, fromCache);
-              initAtlasUpdating(connection.networkAtlas, fromCache);
-              resolve();
-            })
-            .catch((error) => {
-              self.alertSrv.set(self.name, CONNECTION_ERROR_MESSAGES[error], 'error');
-              console.log('');
-              console.log('NetCrunch datasource');
-              console.log(self.name + ': ' + CONNECTION_ERROR_MESSAGES[error]);
-              reject();
-            });
-        } else {
-          reject();
-        }
-      });
-    }
-
-    function initNodesUpdating( networkAtlas, fromCache) {
-
-      function prepareNodeList(networkAtlas) {
-        return networkAtlas.getOrderedNodes()
-          .then(nodes => networkAtlas.addNodesMap(nodes));
+      function prepareNodeList(netAtlas) {
+        return netAtlas.getOrderedNodes()
+          .then(nodes => netAtlas.addNodesMap(nodes));
       }
 
       function updateNodes() {
@@ -73,17 +48,44 @@ class NetCrunchDatasource {
         updateNodes();
       }
 
-      $rootScope.$on('netcrunch-nodes-data-changed(' + self.name + ')', updateNodes);
+      $rootScope.$on(`netcrunch-nodes-data-changed(${self.name})`, updateNodes);
     }
 
-    function initAtlasUpdating( networkAtlas, fromCache) {
+    function initAtlasUpdating(networkAtlas, fromCache) {
 
       if ((fromCache === true) && (networkAtlas.networksReceived === true)) {
         atlasReady(networkAtlas);
       }
 
-      $rootScope.$on('netcrunch-networks-data-changed(' + self.name + ')', function() {
-        atlasReady(networkAtlas);
+      $rootScope.$on(`netcrunch-networks-data-changed(${self.name})`, () => atlasReady(networkAtlas));
+    }
+
+    function initDatasource() {
+      let netCrunchSession;
+
+      return new Promise((resolve, reject) => {
+        if (self.url != null) {
+          netCrunchSession = self.netCrunchAPI.getConnection(self);
+          netCrunchSession
+            .then((connection) => {
+              const fromCache = connection.fromCache;
+              self.netCrunchConnection = connection;
+              initNodesUpdating(connection.networkAtlas, fromCache);
+              initAtlasUpdating(connection.networkAtlas, fromCache);
+              resolve();
+            })
+            .catch((error) => {
+              self.alertSrv.set(self.name, CONNECTION_ERROR_MESSAGES[error], 'error');
+              /* eslint-disable no-console */
+              console.log('');
+              console.log('NetCrunch datasource');
+              console.log(`${self.name}: ${CONNECTION_ERROR_MESSAGES[error]}`);
+              /* eslint-enable no-console */
+              reject();
+            });
+        } else {
+          reject();
+        }
       });
     }
 
@@ -108,54 +110,51 @@ class NetCrunchDatasource {
       }
       return datasourceInitialization;
     };
-
   }
-
-  datasourceReady() {};
 
   testDatasource() {
     return new Promise((resolve) => {
       this.netCrunchAPI.testConnection(this)
         .then(() => {
           resolve({
-            status: "success",
-            message: "Datasource is working",
-            title: "Success" });
+            status: 'success',
+            message: 'Datasource is working',
+            title: 'Success' });
         })
         .catch((error) => {
           resolve({
-            status: "error",
+            status: 'error',
             message: CONNECTION_ERROR_MESSAGES[error],
-            title: "Error" });
+            title: 'Error' });
         });
     });
   }
 
   query(options) {
-    let self = this;
+    const self = this;
 
     function validateCounterData(target) {
-      let nodeName,
-          counterDisplayName,
-          countersAPI = self.netCrunchConnection.counters;
-
-      nodeName = self.getNodeById(target.nodeID)
-                  .then(nodeData => (nodeData != null) ? nodeData.values.Name : null);
-
-      counterDisplayName = self.getCounters(target.nodeID).then((counterList) => {
-        let counterData = countersAPI.findCounterByName(counterList, target.counterName);
-        return (counterData != null) ? counterData.displayName : null;
-      });
+      const
+        countersAPI = self.netCrunchConnection.counters,
+        nodeName = self.getNodeById(target.nodeID).then((nodeData) => {
+          const result = (nodeData != null) ? nodeData.values.Name : null;
+          return result;
+        }),
+        counterDisplayName = self.getCounters(target.nodeID).then((counterList) => {
+          const counterData = countersAPI.findCounterByName(counterList, target.counterName);
+          return (counterData != null) ? counterData.displayName : null;
+        });
 
       return Promise.all([nodeName, counterDisplayName])
         .then((counterData) => {
-          let nodeName = counterData[0],
-              counterDisplayName = counterData[1];
+          const
+            nodeName = counterData[0],              // eslint-disable-line
+            counterDisplayName = counterData[1];    // eslint-disable-line
 
           if ((nodeName != null) && (counterDisplayName != null)) {
             return {
-              nodeName : nodeName,
-              counterDisplayName : counterDisplayName
+              nodeName,
+              counterDisplayName
             };
           }
 
@@ -168,10 +167,10 @@ class NetCrunchDatasource {
     }
 
     function prepareSeriesName(target, counterData) {
-      let seriesName = counterData.nodeName + ' - ' + counterData.counterDisplayName;
+      let seriesName = `${counterData.nodeName} - ${counterData.counterDisplayName}`;
 
       if (target.datasource != null) {
-        seriesName = self.name + ' - ' + seriesName;
+        seriesName = `${self.name} - ${seriesName}`;
       }
       seriesName = target.alias || seriesName;
 
@@ -179,11 +178,11 @@ class NetCrunchDatasource {
     }
 
     function extendSeriesName(baseSeriesName, seriesType) {
-      return baseSeriesName + '\\' + SERIES_TYPES_DISPLAY_NAMES[seriesType];
+      return `${baseSeriesName}\\${SERIES_TYPES_DISPLAY_NAMES[seriesType]}`;
     }
 
-    function prepareSeriesDataQuery (target, range, series) {
-      let trendsAPI = self.netCrunchConnection.trends;
+    function prepareSeriesDataQuery(target, range, series) {
+      const trendsAPI = self.netCrunchConnection.trends;
 
       if (seriesTypesSelected(series) === false) {
         return Promise.resolve([]);
@@ -191,10 +190,10 @@ class NetCrunchDatasource {
 
       return trendsAPI.getCounterTrendData(target.nodeID, target.counterName, range.from, range.to,
                                            range.periodType, range.periodInterval, series)
-        .then((dataPoints) => {
-          return Object.keys(dataPoints.values).map((seriesType) => {
+        .then((dataPoints) => {                                         // eslint-disable-line
+          return Object.keys(dataPoints.values).map((seriesType) => {   // eslint-disable-line
             return {
-              seriesType: seriesType,
+              seriesType,
               dataPoints: {
                 domain: dataPoints.domain,
                 values: dataPoints.values[seriesType]
@@ -204,13 +203,43 @@ class NetCrunchDatasource {
         });
     }
 
-    function prepareQueries(targets, range, rawData) {
-      let result = [];
-      targets.forEach((target) => {
-        let targetDataQuery,
-            series = (rawData === true) ? {avg : true} : target.series;
+    function prepareTargetQuery(target, range, series) {
+      let targetDataQuery = null;
 
-        targetDataQuery = prepareTargetQuery(target, range, series);
+      if ((target.hide !== true) && (target.counterDataComplete === true)) {
+
+        targetDataQuery = validateCounterData(target)
+          .then((counterData) => {
+            let
+              query = null,
+              seriesName,
+              seriesDataQuery,
+              seriesTypes;
+
+            if (counterData != null) {
+              seriesName = prepareSeriesName(target, counterData);
+              query = [Promise.resolve(seriesName)];
+              seriesTypes = (series == null) ? Object.create(null) : series;
+              seriesTypes = NetCrunchDatasource.validateSeriesTypes(seriesTypes);
+              seriesDataQuery = prepareSeriesDataQuery(target, range, seriesTypes);
+              query.push(seriesDataQuery);
+              query = Promise.all(query);
+            }
+
+            return query;
+          });
+      }
+
+      return targetDataQuery;
+    }
+
+    function prepareQueries(targets, range, rawData) {
+      const result = [];
+      targets.forEach((target) => {
+        const
+          series = (rawData === true) ? { avg: true } : target.series,
+          targetDataQuery = prepareTargetQuery(target, range, series);
+
         if (targetDataQuery != null) {
           result.push(targetDataQuery);
         }
@@ -218,46 +247,20 @@ class NetCrunchDatasource {
       return result;
     }
 
-    function prepareTargetQuery (target, range, series) {
-      let targetDataQuery = null;
-
-      if ((target.hide !== true) && (target.counterDataComplete === true)) {
-        targetDataQuery = validateCounterData(target)
-          .then((counterData) => {
-            let query = null,
-                seriesName,
-                seriesDataQuery,
-                seriesTypes;
-
-            if (counterData != null) {
-              seriesName = prepareSeriesName(target, counterData);
-              query = [Promise.resolve(seriesName)];
-              seriesTypes = (series == null) ? Object.create(null) : series;
-              seriesTypes = self.validateSeriesTypes(seriesTypes);
-              seriesDataQuery = prepareSeriesDataQuery(target, range, seriesTypes);
-              query.push(seriesDataQuery);
-              query = Promise.all(query);
-            }
-
-            return query;
-        });
-      }
-
-      return targetDataQuery;
-    }
-
     function prepareChartData(targetsChartData, rawData) {
-      let counterSeries = Object.create(null),
-          trendsAPI = self.netCrunchConnection.trends;
+      const
+        counterSeries = Object.create(null),
+        trendsAPI = self.netCrunchConnection.trends;
 
       counterSeries.data = [];
 
       if ((targetsChartData != null) && (targetsChartData.length > 0)) {
         targetsChartData.forEach((target) => {
-          let baseSeriesName = (target != null) ? target[0] : null,
-              targetSeries = (target != null) ? target[1] : null,
-              extendedSeriesNames = !rawData,
-              seriesName;
+          const
+            baseSeriesName = (target != null) ? target[0] : null,
+            targetSeries = (target != null) ? target[1] : null,
+            extendedSeriesNames = !rawData;
+          let seriesName;
 
           if (target != null) {
             targetSeries.forEach((series) => {
@@ -278,21 +281,23 @@ class NetCrunchDatasource {
       return counterSeries;
     }
 
-    function performQuery(options) {
+    function performQuery(queryOptions) {
 
-      const RAW_TIME_RANGE_EXCEEDED_WARNING_TITLE = 'Time range is too long.',
-            RAW_TIME_RANGE_EXCEEDED_WARNING_TEXT = 'Maximum allowed length of time range for RAW data is ';
+      const
+        RAW_TIME_RANGE_EXCEEDED_WARNING_TITLE = 'Time range is too long.',
+        RAW_TIME_RANGE_EXCEEDED_WARNING_TEXT = 'Maximum allowed length of time range for RAW data is ',
+        trends = self.netCrunchConnection.trends,
+        targets = queryOptions.targets || [],
+        globalOptions = queryOptions.scopedVars || {},
+        rawData = (globalOptions.rawData == null) ? false : globalOptions.rawData,
+        setMaxDataPoints = (globalOptions.setMaxDataPoints == null) ? false : globalOptions.setMaxDataPoints,
+        maxDataPoints = globalOptions.maxDataPoints,
+        rangeFrom = queryOptions.range.from.startOf('minute'),
+        rangeTo = queryOptions.range.to.startOf('minute');
 
-      let trends = self.netCrunchConnection.trends,
-          targets = options.targets || [],
-          globalOptions = options.scopedVars || {},
-          rawData = (globalOptions.rawData == null) ? false : globalOptions.rawData,
-          setMaxDataPoints = (globalOptions.setMaxDataPoints == null) ? false : globalOptions.setMaxDataPoints,
-          maxDataPoints = globalOptions.maxDataPoints,
-          rangeFrom = options.range.from.startOf('minute'),
-          rangeTo = options.range.to.startOf('minute'),
-          range,
-          dataQueries = [];
+      let
+        range,
+        dataQueries = [];
 
       range = trends.prepareTimeRange(rangeFrom, rangeTo, rawData, setMaxDataPoints ? maxDataPoints : null);
 
@@ -300,6 +305,7 @@ class NetCrunchDatasource {
         range = range.result;
         dataQueries = dataQueries.concat(prepareQueries(targets, range, rawData));
       } else {
+        // eslint-disable-next-line
         const ERROR_MESSAGE = RAW_TIME_RANGE_EXCEEDED_WARNING_TEXT + ' ' + range.error.periodInterval + ' ' +
                               range.error.periodName + '.';
         self.alertSrv.set(RAW_TIME_RANGE_EXCEEDED_WARNING_TITLE, ERROR_MESSAGE, 'warning');
@@ -312,31 +318,31 @@ class NetCrunchDatasource {
     try {
       return this.datasourceReady()
         .then(() => performQuery(options));
-    }
-
-    catch(error) {
+    } catch (error) {
       return Promise.reject(error);
     }
   }
 
   getNodeById(nodeID) {
     return this.datasourceReady()
-      .then(() => {
-        return this.nodes.then(nodes => nodes.nodesMap.get(nodeID));
-      });
-  };
+      .then(() => this.nodes.then(nodes => nodes.nodesMap.get(nodeID)));
+  }
 
   getCounters(nodeId, fromCache = true) {
     return this.datasourceReady()
       .then(() => this.netCrunchConnection.counters.getCountersForMonitors(nodeId, fromCache));
-  };
+  }
 
-  validateSeriesTypes(series) {
-    let types = ['min', 'avg', 'max', 'avail', 'delta', 'equal', 'distr' ];
+  static validateSeriesTypes(series) {
+    const
+      types = ['min', 'avg', 'max', 'avail', 'delta', 'equal', 'distr'],
+      buffer = series;
 
-    types.forEach(type => series[type] = (series[type] == null) ? false : series[type]);
-    return series;
-  };
+    types.forEach((type) => {
+      buffer[type] = (buffer[type] == null) ? false : buffer[type];
+    });
+    return buffer;
+  }
 
 }
 
