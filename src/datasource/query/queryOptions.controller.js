d513050d8129579eec3154e2cd1b9c3b2b6f1060
Bogusław Gorczyca <bgorczyca@adrem.com.pl>
2016-12-06 11:37:53 +0100

    Add setDefaults to queryOptions

--- src/datasource/query/queryOptions.controller.js
+++ src/datasource/query/queryOptions.controller.js
@@ -14,13 +14,36 @@ class NetCrunchQueryOptionsController extends QueryCtrl {
   constructor() {
     super();
     this.MAX_SAMPLE_COUNT = this.panelCtrl.datasource.MAX_SAMPLE_COUNT;
+    this.setDefaults();
     this.checkMaxDataPoints();
   }
 
+  setDefaults() {
+    const
+      DEFAULT_QUERY_OPTIONS = {
+        maxDataPoints: this.MAX_SAMPLE_COUNT.DEFAULT,
+        rawData: false,
+        setMaxDataPoints: false
+      },
+      panel = this.panel;
+
+    if (panel.scopedVars == null) {
+      panel.scopedVars = DEFAULT_QUERY_OPTIONS;
+    } else {
+      Object.keys(DEFAULT_QUERY_OPTIONS)
+        .forEach((option) => {
+          if (panel.scopedVars[option] == null) {
+            panel.scopedVars[option] = DEFAULT_QUERY_OPTIONS[option];
+          }
+        });
+    }
+  }
+
   checkMaxDataPoints() {
-    if (!((this.panel.scopedVars.maxDataPoints >= this.MAX_SAMPLE_COUNT.MIN) &&
-          (this.panel.scopedVars.maxDataPoints <= this.MAX_SAMPLE_COUNT.MAX))) {
-      this.panel.scopedVars.maxDataPoints = this.MAX_SAMPLE_COUNT.DEFAULT;
+    const scopedVars = this.panel.scopedVars;
+    if (!((scopedVars.maxDataPoints >= this.MAX_SAMPLE_COUNT.MIN) &&
+          (scopedVars.maxDataPoints <= this.MAX_SAMPLE_COUNT.MAX))) {
+      scopedVars.maxDataPoints = this.MAX_SAMPLE_COUNT.DEFAULT;
     }
   }
 
