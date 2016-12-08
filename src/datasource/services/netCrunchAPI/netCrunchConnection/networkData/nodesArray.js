774e8cda2b1cc06dbf02d09d148f82f7d19a5430
Bogusław Gorczyca <bgorczyca@adrem.com.pl>
2016-12-08 17:45:08 +0100

    Improve NetCrunchNodesArray

--- src/datasource/services/netCrunchAPI/netCrunchConnection/networkData/nodesArray.js
+++ src/datasource/services/netCrunchAPI/netCrunchConnection/networkData/nodesArray.js
@@ -6,11 +6,81 @@
  * found in the LICENSE file.
  */
 
+import { AdremWebWorker } from '../../adrem/module';
+
+const THREAD_WORKER_NODES_NUMBER = 1000;
+
+let sortingWebWorkerSingleton = null;
+
+function sortNodesByNameAndAddress(nodes) {
+
+  function compareAddressIP(addressOne, addressTwo) {
+    const
+      addressOneItems = addressOne.split('.'),
+      addressTwoItems = addressTwo.split('.');
+
+    for (let i = 0, n = Math.max(addressOneItems.length, addressTwoItems.length); i < n; i += 1) {
+      if (addressOneItems[i] !== addressTwoItems[i]) {
+        return (addressOneItems[i] < addressTwoItems[i]) ? -1 : 1;
+      }
+    }
+    return 0;
+  }
+
+  function getNodeProperty(node, propertyName) {
+    return ((node != null) && (node[propertyName] != null)) ? node[propertyName] : '';
+  }
+
+  function compareNodeData(nodeA, nodeB) {
+    const
+      nodeAName = getNodeProperty(nodeA, 'name').toLowerCase(),
+      nodeBName = getNodeProperty(nodeB, 'name').toLowerCase(),
+      nodeAAddress = getNodeProperty(nodeA, 'address'),
+      nodeBAddress = getNodeProperty(nodeB, 'address');
+    let result = 0;
+
+    if ((nodeAName !== '') && (nodeBName !== '')) {
+      result = nodeAName.localeCompare(nodeBName);
+    } else if ((nodeAName === '') && (nodeBName === '')) {
+      result = compareAddressIP(nodeAAddress, nodeBAddress);
+    } else {
+      if (nodeAName !== '') { result = -1; }
+      if (nodeBName !== '') { result = 1; }
+    }
+    return result;
+  }
+
+  return nodes
+    .filter(node => ((node != null) && ((node.id != null) && ((node.name != null) && (node.address != null)))))
+    .sort(compareNodeData);
+
+}
+
+function getSortByNameAndAddressWebWorker() {
+  if (sortingWebWorkerSingleton == null) {
+    const workerBuilder = AdremWebWorker.webWorkerBuilder();
+    workerBuilder.addFunctionCode(sortNodesByNameAndAddress, true);
+    sortingWebWorkerSingleton = workerBuilder.getWebWorker();
+  }
+  return sortingWebWorkerSingleton;
+}
+
 // Babel doesn't support extends from native class like Array, Map, Set
 
 function NetCrunchNodesArray() {}
 
 NetCrunchNodesArray.prototype = Object.create(Array.prototype);
+
+NetCrunchNodesArray.prototype.sortByNameAndAddress = function() {
+  return new Promise((resolve) => {
+    if (this.length < THREAD_WORKER_NODES_NUMBER) {
+      resolve(sortNodesByNameAndAddress(this));
+    } else {
+      getSortByNameAndAddressWebWorker().sortNodesByNameAndAddress(this)
+        .then(nodes => resolve(nodes));
+    }
+  });
+};
 
 export {
   NetCrunchNodesArray
