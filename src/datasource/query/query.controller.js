f0a64b997c2e78af76ef508d0c878b58bf6edc85
Bogusław Gorczyca <bgorczyca@adrem.com.pl>
2016-12-15 16:50:25 +0100

    Add node metric selector

--- src/datasource/query/query.controller.js
+++ src/datasource/query/query.controller.js
@@ -6,19 +6,27 @@
  * found in the LICENSE file.
  */
 
+import '../css/query.editor.css!';            // eslint-disable-line
 import { QueryCtrl } from 'app/plugins/sdk';  // eslint-disable-line
 import { datasourceURL } from '../common';
 
+const
+  PRIVATE_PROPERTIES = {
+    uiSegmentSrv: Symbol('uiSegmentSrv')
+  },
+  DEFAULT_NODE_NAME = 'Select node';
+
 class NetCrunchQueryController extends QueryCtrl {
 
-  constructor() {
+  constructor(uiSegmentSrv) {
     super();
+
+    this[PRIVATE_PROPERTIES.uiSegmentSrv] = uiSegmentSrv;
 
     this.target.series = this.target.series || Object.create(null);
     this.target.showSeriesOptions = this.target.showSeriesOptions || true;
 
-    this.nodes = [];
-
+    this.nodeSegment = this.createDefaultSegment(DEFAULT_NODE_NAME);
   }
 
   targetChanged() {
@@ -27,6 +35,55 @@ class NetCrunchQueryController extends QueryCtrl {
 
   toggleSeries() {
     this.target.showSeriesOptions = !this.target.showSeriesOptions;
+  }
+
+  createDefaultSegment(segmentName) {
+    const segment = {
+      cssClass: 'nc-reset-segment',
+      fake: true,
+      type: 'value',
+      html: `<div class="nc-default-tile">${segmentName}</div>`,
+      value: segmentName
+    };
+    return this[PRIVATE_PROPERTIES.uiSegmentSrv].newSegment(segment);
+  }
+
+  createNodeSegment(node) {
+    const nodeSegmentTemplate = `
+      <div class="nc-node-tile">
+        <img class="nc-node-icon" src=${node.iconUrl}>
+        <div class="nc-node-description">
+          <span class="nc-node-name">${node.name}</span>
+          <span class="nc-node-address">${node.address}</span>
+        </div>
+      </div>        
+    `;
+
+    return this[PRIVATE_PROPERTIES.uiSegmentSrv].newSegment({
+      cssClass: 'nc-reset-segment',
+      fake: true,
+      type: 'value',
+      html: nodeSegmentTemplate,
+      value: NetCrunchQueryController.nodeDisplayValue(node)
+    });
+  }
+
+  getNodes() {
+    return this.datasource
+      .nodes().then(nodes => nodes.all.map(node => this.createNodeSegment(node)));
+  }
+
+  nodeChanged() {
+    this.targetChanged();
+  }
+
+  static nodeDisplayValue(node) {
+    if ((node.name != null) && (node.name !== '')) {
+      return `${node.name}${(((node.address != null) && (node.address !== '')) ? ` (${node.address})` : '')}`;
+    } else if ((node.address != null) && (node.address !== '')) {
+      return node.address;
+    }
+    return '';
   }
 
   static get templateUrl() {
