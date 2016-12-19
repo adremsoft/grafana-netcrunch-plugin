64f39656752d0ce6096ee3fb6bf9597340561a5d
Bogusław Gorczyca <bgorczyca@adrem.com.pl>
2016-12-19 14:12:45 +0100

    Add counter selection

--- src/datasource/query/query.controller.js
+++ src/datasource/query/query.controller.js
@@ -7,40 +7,125 @@
  */
 
 import '../css/query.editor.css!';            // eslint-disable-line
+import '../directives/nc-spinner.directive';
 import { QueryCtrl } from 'app/plugins/sdk';  // eslint-disable-line
 import { datasourceURL } from '../common';
 
 const
   PRIVATE_PROPERTIES = {
     uiSegmentSrv: Symbol('uiSegmentSrv'),
-    nodeMap: Symbol('nodeMap')
+    scope: Symbol('scope'),
+    nodeMap: Symbol('nodeMap'),
+    nodeSegment: Symbol('nodeSegment'),
+    nodeSpinner: Symbol('nodeSpinner'),
+    counterName: Symbol('counterName'),
+    counterSpinner: Symbol('counterSpinner'),
+    counters: Symbol('counters')
   },
-  DEFAULT_NODE_NAME = 'Select node';
+  DEFAULT_NODE_NAME = 'Select node',
+  DEFAULT_COUNTER_DISPLAY_NAME = 'Select counter';
 
 class NetCrunchQueryController extends QueryCtrl {
 
-  constructor(uiSegmentSrv) {
+  constructor(uiSegmentSrv, $scope) {
     super();
 
     this[PRIVATE_PROPERTIES.uiSegmentSrv] = uiSegmentSrv;
+    this[PRIVATE_PROPERTIES.scope] = $scope;
     this[PRIVATE_PROPERTIES.nodeMap] = new Map();
+    this[PRIVATE_PROPERTIES.nodeSegment] = this.createDefaultNodeSegment(DEFAULT_NODE_NAME);
+    this[PRIVATE_PROPERTIES.nodeSpinner] = false;
+    this[PRIVATE_PROPERTIES.counterName] = null;
+    this[PRIVATE_PROPERTIES.counterSpinner] = false;
+    this[PRIVATE_PROPERTIES.counters] = [];
 
-    this.target.series = this.target.series || Object.create(null);
-    this.target.showSeriesOptions = this.target.showSeriesOptions || true;
-    this.localVars = this.target.localVars;
+    this.localVars = Object.create(null);
 
-    this.nodeSegment = this.createDefaultSegment(DEFAULT_NODE_NAME);
+    this.processingNode = true;
+    this.nodeReady = false;
+    this.datasource.nodes().then(() => {
+      this.processingNode = false;
+      this.updateView();
+    });
+
+    this.processingCounter = true;
+    this.counterReady = false;
+
   }
 
-  targetChanged() {
-    this.panelCtrl.refresh();
+  get localVars() {
+    return this.target.localVars;
   }
 
-  toggleSeries() {
-    this.target.showSeriesOptions = !this.target.showSeriesOptions;
+  set localVars(value) {
+    this.target.localVars = value;
   }
 
-  createDefaultSegment(segmentName) {
+  get processingNode() {
+    return this.localVars.processingNode;
+  }
+
+  set processingNode(value) {
+    this.localVars.processingNode = value;
+  }
+
+  get nodeSpinner() {
+    return this[PRIVATE_PROPERTIES.nodeSpinner];
+  }
+
+  get nodeSegment() {
+    return this[PRIVATE_PROPERTIES.nodeSegment];
+  }
+
+  get nodeReady() {
+    return this.localVars.nodeReady;
+  }
+
+  set nodeReady(value) {
+    this.localVars.nodeReady = value;
+  }
+
+  get processingCounter() {
+    return this.localVars.processingCounter;
+  }
+
+  set processingCounter(value) {
+    this.localVars.processingCounter = value;
+  }
+
+  get defaultCounterName() {              // eslint-disable-line
+    return DEFAULT_COUNTER_DISPLAY_NAME;
+  }
+
+  get counterSpinner() {
+    return this[PRIVATE_PROPERTIES.counterSpinner];
+  }
+
+  get counterReady() {
+    return this.localVars.counterReady;
+  }
+
+  set counterReady(value) {
+    this.localVars.counterReady = value;
+  }
+
+  get counterName() {
+    return this[PRIVATE_PROPERTIES.counterName];
+  }
+
+  get counters() {
+    return this[PRIVATE_PROPERTIES.counters];
+  }
+
+  get counterDataComplete() {
+    return this.target.counterDataComplete;
+  }
+
+  set counterDataComplete(value) {
+    this.target.counterDataComplete = value;
+  }
+
+  createDefaultNodeSegment(segmentName) {
     const segment = {
       cssClass: 'nc-reset-segment',
       fake: true,
@@ -52,6 +137,7 @@ class NetCrunchQueryController extends QueryCtrl {
   }
 
   createNodeSegment(node) {
+
     const nodeSegmentTemplate = `
       <div class="nc-node-tile">
         <img class="nc-node-icon" src=${node.iconUrl}>
@@ -59,7 +145,7 @@ class NetCrunchQueryController extends QueryCtrl {
           <span class="nc-node-name">${node.name}</span>
           <span class="nc-node-address">${node.address}</span>
         </div>
-      </div>        
+      </div>
     `;
 
     return this[PRIVATE_PROPERTIES.uiSegmentSrv].newSegment({
@@ -69,6 +155,22 @@ class NetCrunchQueryController extends QueryCtrl {
       html: nodeSegmentTemplate,
       value: NetCrunchQueryController.nodeDisplayValue(node)
     });
+  }
+
+  targetChanged() {
+    this.panelCtrl.refresh();
+  }
+
+  nodeSpinnerChanged(state) {
+    this[PRIVATE_PROPERTIES.nodeSpinner] = state;
+  }
+
+  counterSpinnerChanged(state) {
+    this[PRIVATE_PROPERTIES.counterSpinner] = state;
+  }
+
+  updateView() {
+    this[PRIVATE_PROPERTIES.scope].$apply();
   }
 
   getNodes() {
@@ -83,15 +185,91 @@ class NetCrunchQueryController extends QueryCtrl {
       });
   }
 
-  nodeChanged() {
-    const nodeUpdated = this[PRIVATE_PROPERTIES.nodeMap].has(this.nodeSegment.value);
+  nodeChanged(nodeId = null) {
+    const self = this;
+    let selectedNode;
 
-    this.localVars.nodeUpdated = nodeUpdated;
-
-    if (nodeUpdated) {
-      this.nodeID = this[PRIVATE_PROPERTIES.nodeMap].get(this.nodeSegment.value).id;
-      this.targetChanged();
+    function nodeNotReady() {
+      self.nodeReady = false;
+      self.processingCounter = true;
+      self.counterReady = false;
+      self.counterDataComplete = false;
     }
+
+    function getCounters(nodeId) {        // eslint-disable-line
+      return self.datasource
+        .getCounters(nodeId)
+          .then((countersByMonitors) => {
+            const countersMenu = [];
+
+            Object.keys(countersByMonitors).forEach((monitorId) => {
+              if (monitorId > 0) {
+                const subMenu = countersByMonitors[monitorId]
+                  .counters.map(counter => ({
+                    text: counter.displayName,
+                    value: counter.name
+                  }));
+
+                countersMenu.push({
+                  text: countersByMonitors[monitorId].name,
+                  submenu: subMenu
+                });
+              }
+            });
+
+            return {
+              countersMenu,
+              countersTable: countersByMonitors.table
+            };
+          });
+    }
+
+    if (nodeId != null) {
+      selectedNode = this.datasource
+        .nodes()
+          .then(nodes => ((nodes.all.some(node => (nodeId === node.id))) ? nodeId : null));
+    } else if (this[PRIVATE_PROPERTIES.nodeMap].has(this.nodeSegment.value)) {
+      selectedNode = Promise.resolve(this[PRIVATE_PROPERTIES.nodeMap].get(this.nodeSegment.value).id);
+    } else {
+      selectedNode = Promise.resolve(null);
+    }
+
+    selectedNode.then((selectedNodeId) => {
+      this.target.nodeID = selectedNodeId;
+
+      if (selectedNodeId == null) {
+        nodeNotReady();
+        Object.assign(this[PRIVATE_PROPERTIES.nodeSegment], this.createDefaultNodeSegment(DEFAULT_NODE_NAME));
+        this.targetChanged();
+      } else {
+        this.nodeReady = true;
+        this.processingCounter = true;
+        this.counterReady = false;
+        this.updateView();
+
+        getCounters(selectedNodeId)
+          .then((counters) => {
+            this.processingCounter = false;
+            this[PRIVATE_PROPERTIES.counters] = counters.countersMenu;
+
+            if (counters.countersTable.some(counter => (counter.name === this.target.counterName))) {
+              this[PRIVATE_PROPERTIES.counterName] = this.target.counterName;
+              this.counterReady = true;
+              this.counterDataComplete = true;
+              this.targetChanged();
+            } else {
+              this.updateView();
+            }
+          });
+      }
+    });
+  }
+
+  counterChanged(counter) {
+    this[PRIVATE_PROPERTIES.counterName] = counter.value;
+    this.target.counterName = counter.value;
+    this.counterDataComplete = true;
+    this.targetChanged();
   }
 
   static nodeDisplayValue(node) {
