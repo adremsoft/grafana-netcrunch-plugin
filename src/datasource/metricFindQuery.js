e40dea5e5016bd336d6150aca789518802d13c14
Bogusław Gorczyca <boguslaw@gorczyca.name>
2017-03-28 16:36:35 +0200

    Refactor NetCrunchMetricFindQuery

--- src/datasource/metricFindQuery.js
+++ src/datasource/metricFindQuery.js
@@ -13,42 +13,185 @@ class NetCrunchMetricFindQuery {
     this.query = query;
   }
 
-  nodes(selectors) {
+  process() {
+    const tasks = NetCrunchMetricFindQuery.parseQuery(this.query);
 
-    function createQueryResult(nodes) {
-      return nodes.map((node) => {
-        const ipAddress = (node.address != null) ? `(${node.address})` : '';
-        return {
-          text: `${node.name} ${ipAddress}`,
-          value: node.id
-        };
-      });
+    if (tasks.completeParsed) {
+      return Promise.all([this.datasource.nodes(), this.datasource.atlas()])
+        .then((result) => {
+          const
+            atlas = result[1],
+            allNodes = result[0].all,
+            tasksResult = NetCrunchMetricFindQuery.processQueryTasks(tasks, atlas, allNodes);
+
+          return NetCrunchMetricFindQuery.createQueryResult((tasksResult.success) ? tasksResult.nodes : []);
+        });
     }
 
-    const parameters = selectors.split('.').filter(parameter => parameter !== '');
-
-    return this.datasource
-      .nodes()
-        .then((nodes) => {
-          let result = nodes.all;
-
-          if (parameters.length === 1) {
-            result = nodes.operations.deviceTypeFilter(result, parameters[0]);
-          } else {
-            result = nodes.operations.deviceTypeFilter(result, parameters.join('.'));
-          }
-
-          return createQueryResult(result);
-        });
+    return Promise.resolve(NetCrunchMetricFindQuery.createQueryResult([]));
   }
 
-  process() {
-    const nodes = this.query.match(/^[nN][oO][dD][eE][sS]((\.[\w]+)*)$/);
+  static parseQuery(query) {
+    const
+      namePattern = '\\s*[\\w]+(?:[\\s][\\w]+)*\\s*';
 
-    if (nodes) {
-      return this.nodes(nodes[1]);
+    function getParseResult(result) {
+      if (result != null) {
+        return {
+          parameter: result[1],
+          queryRest: result[2]
+        };
+      }
+      return null;
     }
-    return Promise.resolve([]);
+
+    function parseNodes(nodesQuery) {
+      return getParseResult((nodesQuery || '').match(/^(nodes)(.*)$/i));
+    }
+
+    function parseDeviceTypeItem(deviceTypeItemQuery) {
+      const
+        deviceTypes = 'windows\\.server|windows\\.workstation|windows|linux|bsd|macos|solaris|esx|xenserver' +
+                      '|unix|novell|ibm',
+        result = (deviceTypeItemQuery || '').match(new RegExp(`^\\.(${deviceTypes})(.*)$`, 'i'));
+      return getParseResult(result);
+    }
+
+    function parseMapItem(mapItemQuery) {
+      const
+        mapPattern = `\\.(?:map)\\("(${namePattern})"\\)`,
+        result = (mapItemQuery || '').match(new RegExp(`^${mapPattern}(.*)$`, 'i'));
+
+      return getParseResult(result);
+    }
+
+    function parseMonitoringPack(monitoringPackItemQuery) {
+      const
+        monitoringPackPattern = `\\.(?:monitoringPack)\\("(${namePattern})"\\)`,
+        result = (monitoringPackItemQuery || '').match(new RegExp(`^${monitoringPackPattern}(.*)$`, 'i'));
+
+      return getParseResult(result);
+    }
+
+    function getTask(taskName, taskParameter) {
+      return {
+        name: taskName,
+        parameter: taskParameter
+      };
+    }
+
+    function parseNodesFilterQuery(nodesFilterQuery) {
+      const
+        parsingExpressions = [
+          { parseExpression: parseDeviceTypeItem, taskName: 'deviceTypeFilter' },
+          { parseExpression: parseMapItem, taskName: 'mapFilter' },
+          { parseExpression: parseMonitoringPack, taskName: 'monitoringPackFilter' }
+        ],
+        tasks = [];
+      let
+        queryToParse,
+        parsingSuccessful;
+
+      queryToParse = nodesFilterQuery;
+      parsingSuccessful = true;
+      while ((queryToParse !== '') && (parsingSuccessful)) {
+        parsingSuccessful = parsingExpressions.some((parsingExpression) => {    // eslint-disable-line no-loop-func
+          const parseResult = parsingExpression.parseExpression(queryToParse);
+          if (parseResult != null) {
+            tasks.push(getTask(parsingExpression.taskName, parseResult.parameter));
+            queryToParse = parseResult.queryRest;
+            return true;
+          }
+          return false;
+        });
+      }
+
+      tasks.completeParsed = parsingSuccessful;
+      tasks.unparsedQuery = queryToParse;
+      return tasks;
+    }
+
+    let parseResult = parseNodes(query);
+
+    if (parseResult != null) {
+      return parseNodesFilterQuery(parseResult.queryRest);
+    }
+    parseResult = [];
+    parseResult.completeParsed = false;
+    parseResult.unparsedQuery = query;
+
+    return parseResult;
+  }
+
+  static processQueryTasks(tasks, atlas, nodes) {
+
+    function getFilterResult(success, processedNodes, unprocessedTasks) {
+      return {
+        success,
+        nodes: processedNodes,
+        tasks: unprocessedTasks
+      };
+    }
+
+    function deviceTypeFilter(tasksToProcess, nodesToFilter) {
+      const currentTask = tasksToProcess.shift();
+      return getFilterResult(
+        true,
+        nodesToFilter.filter(node => node.checkDeviceType(currentTask.parameter)),
+        tasksToProcess
+      );
+    }
+
+    function mapFilter(tasksToProcess, nodesToFilter) {
+      const currentTask = tasksToProcess.shift();
+      return getFilterResult(
+        true,
+        nodesToFilter,
+        tasksToProcess
+      );
+    }
+
+    function monitoringPackFilter(tasksToProcess, nodesToFilter) {
+      const currentTask = tasksToProcess.shift();
+      return getFilterResult(
+        true,
+        nodesToFilter,
+        tasksToProcess
+      );
+    }
+
+    const
+      filters = { deviceTypeFilter, mapFilter, monitoringPackFilter };
+    let
+      processingSucceed = true,
+      processedTasks = tasks,
+      processedNodes = nodes,
+      processingResult;
+
+    while (processingSucceed && (processedTasks.length > 0)) {
+      processingResult = filters[tasks[0].name](processedTasks, processedNodes, atlas);
+      processingSucceed = processingResult.success;
+      processedTasks = processingResult.tasks;
+
+      if (processingSucceed) {
+        processedNodes = processingResult.nodes;
+      }
+    }
+
+    return {
+      success: processingSucceed,
+      nodes: processedNodes
+    };
+  }
+
+  static createQueryResult(nodes) {
+    return (nodes || []).map((node) => {
+      const ipAddress = (node.address != null) ? `(${node.address})` : '';
+      return {
+        text: `${node.name} ${ipAddress}`,
+        value: node.id
+      };
+    });
   }
 
 }
